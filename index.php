cb5bc5c307adc4c76d30a37a3f8eb32a15132124
Noel De Martin <noel@moodle.com>
2022-06-28 11:32:22 +0200

    Showcase broken behaviour of table checks in Behat

--- index.php
+++ index.php
@@ -1,2 +1,93 @@
 <h1>Sandbox Plugin</h1>
-<p>Modify this file to start tinkering</p>
+
+<h2>Users</h2>
+<table id="users">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Role</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>John Doe</td>
+            <td>Student</td>
+        </tr>
+        <tr>
+            <td>Amy Doe</td>
+            <td>Teacher</td>
+        </tr>
+    </tbody>
+</table>
+
+
+<h2>Competencies</h2>
+
+<table class="competencies">
+    <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Proficient</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <a href="#">Salads</a>
+                <em>salads</em>
+                <br>
+                <span>Path:&nbsp;</span>
+                <nav>
+                    <small>
+                        <a href="#">
+                            Cookery
+                        </a>
+                        /
+                    </small>
+                </nav>
+
+            </td>
+            <td>Good</td>
+            <td>Yes</td>
+        </tr>
+        <tr>
+            <td>
+                <a href="#">Desserts</a>
+                <em>desserts</em>
+                <br>
+                <span>Path:&nbsp;</span>
+                <nav>
+                    <small>
+                        <a href="#">
+                            Cookery
+                        </a>
+                        /
+                    </small>
+                </nav>
+
+            </td>
+            <td>Great</td>
+            <td>Yes</td>
+        </tr>
+        <tr>
+            <td>
+                <a href="#">Cakes</a>
+                <em>cakes</em>
+                <br>
+                <span>Path:&nbsp;</span>
+                <nav>
+                    <small>
+                        <a href="#">
+                            Cookery
+                        </a>
+                        /
+                    </small>
+                </nav>
+
+            </td>
+            <td>Great</td>
+            <td>Yes</td>
+        </tr>
+    </tbody>
+</table>
--- tests/behat/sandbox.feature
+++ tests/behat/sandbox.feature
@@ -6,3 +6,33 @@ Feature: Sandbox pugin.
 
   Scenario: It works.
     Then I should see "Sandbox Plugin"
+
+  Scenario: It has a table that does not exist (this should fail).
+    Then the following should exist in the "doesnotexist" table:
+      | Name             |
+      | thisdoesnotexist |
+
+  Scenario: It has a table of users (this should pass).
+    Then the following should exist in the "users" table:
+      | Name     |
+      | John Doe |
+      | Amy Doe  |
+
+  Scenario: It has a table of users with roles (this should pass).
+    Then the following should exist in the "users" table:
+      | Name     | Role    |
+      | John Doe | Student |
+      | Amy Doe  | Teacher |
+
+  Scenario: It has a table of user names with roles (this should pass).
+    Then the following should exist in the "users" table:
+      | Name | Role    |
+      | John | Student |
+      | Amy  | Teacher |
+
+  Scenario: It has a table of competencies (this should pass).
+    Then the following should exist in the "competencies" table:
+      | Name                              | Rating | Proficient |
+      | Salads salads Path: Cookery /     | Good   | Yes        |
+      | Desserts desserts Path: Cookery / | Great  | Yes        |
+      | Cakes cakes Path: Cookery /       | Great  | Yes        |
