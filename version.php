a64bf823f12860c76691e0b626f48f3dfe7a59df
Noel De Martin <noel@moodle.com>
2022-07-04 10:23:12 +0200

    Accept older Moodle versions

--- version.php
+++ version.php
@@ -5,5 +5,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_sandbox';
 $plugin->release = '0.0.0';
 $plugin->version = 2022062800;
-$plugin->requires = 2022041900;
+$plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_ALPHA;
